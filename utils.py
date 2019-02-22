1eac17311e5b7a02226eeffc11c4aaf253a62cee
Pawel Morawiecki <pawel.morawiecki@gmail.com>
2019-02-22 11:09:19 +0100

    more helper functions added

--- utils.py
+++ utils.py
@@ -74,9 +74,10 @@ def interval_based_bound(model, c, bounds, idx):
     return (cW.clamp(min=0) @ l[idx].t() + cW.clamp(max=0) @ u[idx].t() + cb[:,None]).t()
 
 
-def epoch_robust_bound(loader, model, epsilon, device, kappa, opt=None):
+def epoch_robust_bound(loader, model, epsilon_schedule, device, kappa_schedule, batch_counter, opt=None):
     robust_err = 0
-    total_robust_loss = 0  
+    total_robust_loss = 0
+    total_combined_loss = 0
     
     C = [-torch.eye(10).to(device) for _ in range(10)]
     for y0 in range(10):
@@ -90,7 +91,7 @@ def epoch_robust_bound(loader, model, epsilon, device, kappa, opt=None):
         fit_loss = nn.CrossEntropyLoss()(yp,y)
     
         ###### robust loss calculation ######
-        initial_bound = (X - epsilon, X + epsilon)
+        initial_bound = (X - epsilon_schedule[batch_counter], X + epsilon_schedule[batch_counter])
         bounds = bound_propagation(model, initial_bound)
         robust_loss = 0
         for y0 in range(10):
@@ -103,11 +104,91 @@ def epoch_robust_bound(loader, model, epsilon, device, kappa, opt=None):
         total_robust_loss += robust_loss.item() * X.shape[0]  
     
         ###### combined losss ######
-        combined_loss = kappa*fit_loss + (1-kappa)*robust_loss
+        combined_loss = kappa_schedule[batch_counter]*fit_loss + (1-kappa_schedule[batch_counter])*robust_loss
+        total_combined_loss += combined_loss.item()
         
+        batch_counter +=1
+        
+      
         if opt:
             opt.zero_grad()
             combined_loss.backward()
             opt.step()
             
-    return robust_err / len(loader.dataset), total_robust_loss / len(loader.dataset)
+    return robust_err / len(loader.dataset), total_combined_loss / len(loader.dataset)
+
+
+def epoch_calculate_robust_err (loader, model, epsilon, device):
+    robust_err = 0.0
+    
+    C = [-torch.eye(10).to(device) for _ in range(10)]
+    for y0 in range(10):
+        C[y0][y0,:] += 1
+
+
+    for X,y in loader:
+        X,y = X.to(device), y.to(device)
+        
+        initial_bound = (X - epsilon, X + epsilon)
+        bounds = bound_propagation(model, initial_bound)
+
+        for y0 in range(10):
+            if sum(y==y0) > 0:
+                lower_bound = interval_based_bound(model, C[y0], bounds, y==y0)                
+                robust_err += (lower_bound.min(dim=1)[0] < 0).sum().item() #increment when true label is not winning       
+        
+    return robust_err / len(loader.dataset)
+        
+        
+
+
+def generate_kappa_schedule_MNIST():
+
+    kappa_schedule = 2000*[1] # warm-up phase
+    kappa_value = 1.0
+    step = 0.5/58000
+    
+    for i in range(58000):
+        kappa_value = kappa_value - step
+        kappa_schedule.append(kappa_value)
+    
+    return kappa_schedule
+
+def generate_epsilon_schedule_MNIST(epsilon_train):
+    
+    epsilon_schedule = []
+    step = epsilon_train/10000
+            
+    for i in range(10000):
+        epsilon_schedule.append(i*step) #ramp-up phase
+    
+    for i in range(50000):
+        epsilon_schedule.append(epsilon_train)
+        
+    return epsilon_schedule
+
+
+def generate_kappa_schedule_CIFAR():
+
+    kappa_schedule = 10000*[1] # warm-up phase
+    kappa_value = 1.0
+    step = 0.5/340000
+    
+    for i in range(340000):
+        kappa_value = kappa_value - step
+        kappa_schedule.append(kappa_value)
+    
+    return kappa_schedule
+
+def generate_epsilon_schedule_CIFAR(epsilon_train):
+    
+    epsilon_schedule = []
+    step = epsilon_train/150000
+            
+    for i in range(150000):
+        epsilon_schedule.append(i*step) #ramp-up phase
+    
+    for i in range(200000):
+        epsilon_schedule.append(epsilon_train)
+        
+    return epsilon_schedule 
